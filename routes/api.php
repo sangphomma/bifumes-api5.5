1ce2711dbd4aeffa5c2d3c7f5fd38dc23a4ad280
sangphomma <87381359+sangphomma@users.noreply.github.com>
2023-09-27 20:29:16 +0700

    Create make:model -a

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::apiResource('/products', ProductController::class) ;
+//สำหรบ Review
+Route::group(['prefix'=>'products'], function(){
+    Route::apiResource('/{product}/reviews', ReviewController::class) ;
+}) ;
